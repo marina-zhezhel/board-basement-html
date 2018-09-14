34aba1024f84c90e977b2aca72d50751e534b630
Maryna <maryna.zhezhel@gmail.com>
2018-09-14 13:37:17 +0000

    Add project board-basement: html,css

---
+++ index.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>The Board Basement</title>
+<link
+	href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:400,500,700"
+	rel="stylesheet">
+<link
+	href="https://fonts.googleapis.com/css?family=Barlow:300i,400,500,600,700"
+	rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans"
+	rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Permanent+Marker"
+	rel="stylesheet">
+<link rel="stylesheet/less" type="text/css" href="css/styles.less" />
+</head>
+<body>
+	<header class="page-header">
+		<div class="header-border"></div>
+		<div class="header-top">
+			<div class="header-left-wrapper">
+				<a class="main-header-logo" href="#"> <img
+					src="images/header-logo.png" width="220.81" height="69.68"
+					alt="Logo The Board Basement">
+				</a>
+				<form class="minisearch" id="search_mini_form"
+					action="http://magento-test/catalogsearch/result/" method="get">
+					<div class="field search">
+						<label class="label visually-hidden" for="search"
+							data-role="minisearch-label"> <span>Search</span>
+						</label> <input id="search" type="text" value=""
+							placeholder="Search entire store here..."
+							class="input-text input-search" maxlength="128">
+					</div>
+				</form>
+			</div>
+			<div class="header-right-wrapper">
+				<p class="main-phone">01392 275 256</p>
+				<div class="authorization">
+					<a href="#"> <img src="images/authorization.png" width="21.53"
+						height="22.7" alt="Authorization">
+					</a>
+				</div>
+				<div class="header-reviews">
+					<a href="#"> <img src="images/header-reviews.png" width="21.53"
+						height="22.7" alt="Reviews">
+					</a>
+				</div>
+				<div class="header-favorite">
+					<a href="#"> <img src="images/header-favorite.png" width="21.53"
+						height="22.7" alt="Favorite">
+					</a>
+				</div>
+				<div class="minicart-wrapper">
+					<a href="#"> <img src="images/minicart.png" width="21.53"
+						height="22.7" alt="Minicart">
+					</a>
+				</div>
+			</div>
+		</div>
+		<nav class="main-navigation">
+			<ul class="site-navigation">
+				<li><a href="#">NEW STUFF</a></li>
+				<li><a href="#">SNOW</a></li>
+				<li><a href="#">WAKE</a></li>
+				<li><a href="#">SURF</a></li>
+				<li><a href="#">SKATE</a></li>
+				<li><a href="#">BIKE</a></li>
+				<li><a href="#">EYEWEAR</a></li>
+				<li><a href="#">FOOTWEAR</a></li>
+				<li><a href="#">BAGS & LUGGAGE</a></li>
+				<li><a href="#">MENS</a></li>
+				<li><a href="#">WOMENS</a></li>
+				<li><a href="#">KIDS</a></li>
+				<li><a href="#">CLEARANCE</a></li>
+				<li><a href="#">BRANDS</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main class="page-main">
+	<h1 class="visually-hidden">The Board Basement</h1>
+	<section class="banners">
+		<div class="banners-wrapper">
+			<h2 class="visually-hidden">Баннеры</h2>
+			<div class="banner-1">
+				<img src="images/wakeboarding.jpg" width="1132" height="703.06">
+				<p class="banner-1-label-1">WAKE UP</p>
+				<p class="banner-1-label-2">2018 BOARDS NOW IN STOCK</p>
+				<a href="#" class="button shop-banner-1">SHOP NOW</a>
+			</div>
+			<div class="banners-right-wrapper">
+				<div class="banner-2">
+					<img src="images/banner-2.jpg" width="554.7" height="341.73">
+					<p class="banner-2-label-1">
+						PACK<br>IT IN
+					</p>
+					<a href="#" class="button shop-banner-2">SHOP NOW</a>
+				</div>
+				<div class="banner-3">
+					<img src="images/banner-3.jpg" width="554.7" height="341.73">
+					<p class="banner-3-label-1">BONFIRE</p>
+					<p class="banner-3-label-2">TECHNICAL OUTERWEAR</p>
+					<a href="#" class="button button shop-banner-3">SHOP NOW</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="categories">
+		<h2 class="categories-title">CATEGORIES</h2>
+		<div class="categories-wrapper">
+
+			<a class="category-item" href="#"><span class="category-item-wrapper">
+					<img src="images/snowboard_ski.jpg" width="329.03" height="136.43">
+			</span> <span class="category-label">SNOWBOARD & SKI</span></a> <a
+				class="category-item" href="#"> <span class="category-item-wrapper"><img
+					src="images/surf.jpg" width="329.03" height="136.43"></span> <span
+				class="category-label">SURF</span>
+			</a> <a class="category-item" href="#"><span
+				class="category-item-wrapper"> <img src="images/wake.jpg"
+					width="329.03" height="136.43"></span> <span class="category-label">WAKE</span>
+			</a> <a class="category-item" href="#"> <span
+				class="category-item-wrapper"><img src="images/skate.jpg"
+					width="329.03" height="136.43"></span> <span class="category-label">SKATE</span>
+			</a> <a class="category-item" href="#"> <span
+				class="category-item-wrapper"><img src="images/bike.jpg"
+					width="329.03" height="136.43"></span> <span class="category-label">BIKE</span>
+			</a> <a class="category-item" href="#"> <span
+				class="category-item-wrapper"><img src="images/footwear.jpg"
+					width="329.03" height="136.43"></span> <span class="category-label">FOOTWEAR</span>
+			</a> <a class="category-item" href="#"> <span
+				class="category-item-wrapper"><img src="images/bags_luggage.jpg"
+					width="329.03" height="136.43"></span> <span class="category-label">BAGS
+					& LUGGAGE</span>
+			</a> <a class="category-item" href="#"> <span
+				class="category-item-wrapper"><img src="images/mens.jpg"
+					width="329.03" height="136.43"> </span><span class="category-label">MENS</span>
+			</a> <a class="category-item" href="#"> <span
+				class="category-item-wrapper"><img src="images/womens.jpg"
+					width="329.03" height="136.43"></span> <span class="category-label">WOMENS</span>
+			</a> <a class="category-item" href="#"> <span
+				class="category-item-wrapper"><img src="images/clearance.jpg"
+					width="329.03" height="136.43"> </span><span class="category-label">CLEARANCE</span>
+			</a>
+		</div>
+	</section>
+	<section class="new-stuff">
+		<h2 class="new-stuff-title">NEW STUFF</h2>
+		<div class="cycle-slideshow" data-cycle-fx=carousel
+			data-cycle-timeout=5000 data-cycle-carousel-visible=6
+			data-cycle-pager="#pager" data-cycle-carousel-fluid=true
+			data-cycle-slides="div.item-wrapper">
+			<div class="item-wrapper">
+				<div class="new-stuff-item">
+					<div class="new-stuff-wrapper">
+						<img class="new-stuff-img" src="images/new-stuff-1.png"
+							width="267.26" height="288.45">
+						<p class="mark-sale">SALE</p>
+					</div>
+					<p class="item-info">
+						<span class="name-item">K2</span> Northern Lite Womens Split
+						Snowboard Package 152Cm 2018
+					</p>
+					<div class="prise-wrapper">
+						<p class="item-prise">£123.45</p>
+						<p class="item-prise-rrp">RRP £234.56</p>
+					</div>
+					<div class="reviews-favorite-wrapper">
+						<a class="item-reviews"><img src="images/item-reviews.png"
+							width="21.93" height="20.96"></a> <a class="item-favorite"><img
+							src="images/item-favorite.png" width="19.14" height="17.72"></a>
+					</div>
+				</div>
+			</div>
+			<div class="item-wrapper">
+				<div class="new-stuff-item">
+					<div class="new-stuff-wrapper">
+						<img class="new-stuff-img" src="images/new-stuff-2.png"
+							width="267.26" height="288.45"> <img class="free-postage"
+							src="images/free-postage.png" width="68.08" height="58.96">
+						<p class="mark-new">NEW IN</p>
+					</div>
+					<p class="item-info">
+						<span class="name-item">Dakine</span> Revival Kit S Cortez
+					</p>
+					<div class="prise-wrapper">
+						<p class="item-prise">£123.45</p>
+						<p class="item-prise-rrp">RRP £234.56</p>
+					</div>
+					<div class="reviews-favorite-wrapper">
+						<a class="item-reviews"><img src="images/item-reviews.png"
+							width="21.93" height="20.96"></a> <a class="item-favorite"><img
+							src="images/item-favorite.png" width="19.14" height="17.72"></a>
+					</div>
+				</div>
+			</div>
+			<div class="item-wrapper">
+				<div class="new-stuff-item">
+					<div class="new-stuff-wrapper">
+						<img class="new-stuff-img" src="images/new-stuff-3.png"
+							width="267.26" height="288.45">
+					</div>
+					<p class="item-info">
+						<span class="name-item">Oakley</span> Fall Line Goggle 2018
+						Canteen Jade Prizm Jade
+					</p>
+					<div class="prise-wrapper">
+						<p class="item-prise">£123.45</p>
+						<p class="item-prise-rrp">RRP £234.56</p>
+					</div>
+					<div class="reviews-favorite-wrapper">
+						<a class="item-reviews"><img src="images/item-reviews.png"
+							width="21.93" height="20.96"></a> <a class="item-favorite"><img
+							src="images/item-favorite.png" width="19.14" height="17.72"></a>
+					</div>
+				</div>
+			</div>
+			<div class="item-wrapper">
+				<div class="new-stuff-item">
+					<div class="new-stuff-wrapper">
+						<img class="new-stuff-img" src="images/new-stuff-4.png"
+							width="267.26" height="288.45">
+					</div>
+					<p class="item-info">
+						<span class="name-item">Dakine</span> Mens Seaboard Hat Black
+					</p>
+					<div class="prise-wrapper">
+						<p class="item-prise">£123.45</p>
+						<p class="item-prise-rrp">RRP £234.56</p>
+					</div>
+					<div class="reviews-favorite-wrapper">
+						<a class="item-reviews"><img src="images/item-reviews.png"
+							width="21.93" height="20.96"></a> <a class="item-favorite"><img
+							src="images/item-favorite.png" width="19.14" height="17.72"></a>
+					</div>
+				</div>
+			</div>
+			<div class="item-wrapper">
+				<div class="new-stuff-item">
+					<div class="new-stuff-wrapper">
+						<img class="new-stuff-img" src="images/new-stuff-5.png"
+							width="267.26" height="288.45">
+					</div>
+					<p class="item-info">Bent Metal Transfer Snowboard Bindings 2018</p>
+					<div class="prise-wrapper">
+						<p class="item-prise">£123.45</p>
+						<p class="item-prise-rrp">RRP £234.56</p>
+					</div>
+					<div class="reviews-favorite-wrapper">
+						<a class="item-reviews"><img src="images/item-reviews.png"
+							width="21.93" height="20.96"></a> <a class="item-favorite"><img
+							src="images/item-favorite.png" width="19.14" height="17.72"></a>
+					</div>
+				</div>
+			</div>
+			<div class="item-wrapper">
+				<div class="new-stuff-item">
+					<div class="new-stuff-wrapper">
+						<img class="new-stuff-img" src="images/new-stuff-6.png"
+							width="267.26" height="288.45">
+					</div>
+					<p class="item-info">Evolve Skateboards Bamboo Gtx Series All
+						Terrain - Black Motors</p>
+					<div class="prise-wrapper">
+						<p class="item-prise">£123.45</p>
+						<p class="item-prise-rrp">RRP £234.56</p>
+					</div>
+					<div class="reviews-favorite-wrapper">
+						<a class="item-reviews"><img src="images/item-reviews.png"
+							width="21.93" height="20.96"></a> <a class="item-favorite"><img
+							src="images/item-favorite.png" width="19.14" height="17.72"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="cycle-pager" id=pager></div>
+		<div class="send-new-stuff-wrapper">
+			<a href="#" class="button send-new-stuff">VIEW ALL NEW STUFF</a>
+		</div>
+	</section>
+	<section class="brand">
+		<h2 class="brand-title">SHOP BY BRAND</h2>
+		<div class="brand-wrapper">
+			<img class="brand-item" src="images/brand-1.png" width="164"
+				height="56" alt="Acme Co"> <img class="brand-item"
+				src="images/brand-2.png" width="164" height="56" alt="Acme Co"> <img
+				class="brand-item" src="images/brand-3.png" width="164" height="56"
+				alt="Acme Co"> <img class="brand-item" src="images/brand-4.png"
+				width="164" height="56" alt="Acme Co"> <img class="brand-item"
+				src="images/brand-5.png" width="164" height="56" alt="Acme Co"> <img
+				class="brand-item" src="images/brand-6.png" width="164" height="56"
+				alt="Acme Co"> <img class="brand-item" src="images/brand-7.png"
+				width="164" height="56" alt="Acme Co"> <img class="brand-item"
+				src="images/brand-8.png" width="164" height="56" alt="Acme Co"> <img
+				class="brand-item" src="images/brand-9.png" width="164" height="56"
+				alt="Acme Co"> <img class="brand-item" src="images/brand-10.png"
+				width="164" height="56">
+		</div>
+		<div class="send-brands-wrapper">
+			<a href="#" class="button send-brands">VIEW ALL BRANDS</a>
+		</div>
+	</section>
+	</main>
+	<footer class="page-footer">
+		<div class="footer-wrapper">
+			<div class="footer-top-wrapper">
+				<section class="footer-info">
+					<h2 class="visually-hidden">Store address</h2>
+					<a class="footer-logo"> <img src="images/footer-logo.png"
+						width="220.81" height="69.68" alt="Logo The Board Basement">
+					</a>
+					<p class="info-text">
+						<b>WE ARE OPEN:</b> 9:00 - 17:00 MON - SAT We are closed on
+						Sundays
+					</p>
+					<p class="info-text">
+						<b>OUR ADDRESS IS:</b><br>The Board Basement, 4 Bridford Road,
+						Marsh Barton, Exeter, Devon, EX2 8QX, United Kingdom
+					</p>
+					<p class="info-text">
+						<b>OUR PHONE NUMBER IS:</b>01392 275 256
+					</p>
+				</section>
+				<section class="page-social-wrapper">
+					<h2 class="visually-hidden">Store navigation</h2>
+					<nav class="page-navigation">
+						<ul class="page-navigation-wrapper">
+							<li class="footer-page-item"><a href="#">ABOUT US</a></li>
+							<li class="footer-page-item"><a href="#">CONTACT US</a></li>
+							<li class="footer-page-item"><a href="#">RIDE WITH US</a></li>
+						</ul>
+					</nav>
+					<ul class="social-wrapper">
+						<li><a class="social-button" href="#"><span
+								class="visually-hidden">Twitter</span><img
+								src="images/social-1.png" width="41.37" height="47.76"
+								alt="Twitter"> </a></li>
+						<li><a class="social-button" href="#"><span
+								class="visually-hidden">Facebook</span><img
+								src="images/social-2.png" width="41.37" height="47.76"
+								alt="Facebook"></a></li>
+						<li><a class="social-button" href="#"><span
+								class="visually-hidden">Instagram</span><img
+								src="images/social-3.png" width="41.37" height="47.76"
+								alt="Instagram"></a></li>
+						<li><a class="social-button" href="#"><span
+								class="visually-hidden">Pinterest</span><img
+								src="images/social-4.png" width="41.37" height="47.76"
+								alt="Pinterest"></a></li>
+						<li><a class="social-button" href="#"><span
+								class="visually-hidden">Tumblr</span><img
+								src="images/social-5.png" width="41.37" height="47.76"
+								alt="Tumblr"></a></li>
+					</ul>
+				</section>
+				<section class="services-wrapper">
+					<h2 class="services-title">CUSTOMER SERVICES</h2>
+					<nav class="services-navigation">
+						<ul class="services-navigation-wrapper">
+							<li class="service-item"><a href="#">DELIVERY</a></li>
+							<li class="service-item"><a href="#">RETURNS</a></li>
+							<li class="service-item"><a href="#">ORDER TRACKING</a></li>
+							<li class="service-item"><a href="#">IN-STORE SIGN UP</a></li>
+							<li class="service-item"><a href="#">HELP</a></li>
+						</ul>
+					</nav>
+				</section>
+				<section class="footer-payment">
+					<h2 class="visually-hidden">Payment</h2>
+					<div class="reviews-wrapper">
+						<img class="reviews-img" src="images/reviews.png" width="412"
+							height="93.22" alt="Reviews">
+					</div>
+					<div class="payment-wrapper">
+						<div class="payment-title-wrapper">
+							<img class="payment-title-img" src="images/payment-title.png"
+								width="13.55" height="16.26">
+							<p class="payment-title">SECURE PAYMENT</p>
+						</div>
+						<ul class="payment-list">
+							<li class="payment-item"><img class="payment-item-img"
+								src="images/payment-1.jpg" width="63.81" height="40.51"
+								alt="Visa"></li>
+							<li class="payment-item"><img class="payment-item-img"
+								src="images/payment-2.jpg" width="63.81" height="40.51"
+								alt="MasterCard"></li>
+							<li class="payment-item"><img class="payment-item-img"
+								src="images/payment-3.jpg" width="63.81" height="40.51"
+								alt="PayPal"></li>
+							<li class="payment-item"><img class="payment-item-img"
+								src="images/payment-4.jpg" width="63.81" height="40.51"
+								alt="Visa Electron"></li>
+							<li class="payment-item"><img class="payment-item-img"
+								src="images/payment-5.jpg" width="63.81" height="40.51"
+								alt="Sage"></li>
+						</ul>
+					</div>
+				</section>
+			</div>
+			<div class="footer-bottom-wrapper">
+				<section class="policy-wrapper">
+					<h2 class="visually-hidden">Policy</h2>
+					<nav class="policy-navigation">
+						<ul class="policy-navigation-wrapper">
+							<li class="policy-item"><a href="#">PRIVACY POLICY</a></li>
+							<li class="policy-item"><a href="#">COOKIE POLICY</a></li>
+							<li class="policy-item"><a href="#">TERMS & CONDITIONS</a></li>
+						</ul>
+					</nav>
+					<p class="copyright">Copyright © 2018 The Board Basement. All
+						Rights Reserved</p>
+				</section>
+			</div>
+		</div>
+		<div class="footer-site-by">
+				<p>SITE BY </p>
+				<img src="images/kodal.png" width="52.73" height="12.21" alt="Sage">
+			</div>
+	</footer>
+	<script src="js/less.min.js" type="text/javascript"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/jquery.cycle2.js"></script>
+	<script src="js/jquery.cycle2.carousel.min.js"></script>
+	<script src="js/board-basement.min.js" async></script>
+</body>
+</html>
